ff27830a4d5f41ff00675f2d255cccf6ddf31a3d
Roman Gardukevich <roman.gardukevich@mapbox.com>
2022-09-02 16:15:53 +0300

    Prefix public-sync branches with 'public/'

--- scripts/public-sync/ci-create-pr-from-public-repo.sh
+++ scripts/public-sync/ci-create-pr-from-public-repo.sh
@@ -2,6 +2,7 @@
 
 set -v
 set -euo pipefail
+set -x
 
 if ! command -v gh &> /dev/null
 then
@@ -10,6 +11,7 @@ then
 fi
 
 readonly BRANCH=$1
+ORIGIN_BRANCH="public/$BRANCH"
 
 if [[ -z $BRANCH ]]
 then
@@ -30,15 +32,15 @@ COMMIT_SHA=$(git rev-parse --abbrev-ref HEAD)
 readonly MERGE_BRANCH=ci-${PRIVATE_BRANCH}-${BRANCH}-merge
 readonly TITLE="${PRIVATE_BRANCH} to ${BRANCH} merge."
 BODY=$(cat << END
-This PR is created automatically by CircleCI to sync ${PRIVATE_BRANCH} branch with ${BRANCH} after update.
+This PR is created automatically by CircleCI to sync private ``${PRIVATE_BRANCH}`` branch with public ``${BRANCH}`` branch after update.
 END
 )
 
-git fetch origin "$BRANCH"
+git fetch origin "$ORIGIN_BRANCH"
 
 # private release branch may be fresh and not exist yet
 # in this case we cut a new branch from the latest internal and push it afterwards
-PRIVATE_BRANCH_EXISTS=$(git ls-remote origin "$PRIVATE_BRANCH" | wc -l)
+PRIVATE_BRANCH_EXISTS=$(git ls-remote origin "$PRIVATE_BRANCH" | grep -c "heads/$PRIVATE_BRANCH")
 if [[ $PRIVATE_BRANCH_EXISTS -eq 0 ]]; then
   echo "$PRIVATE_BRANCH doesn't exist, create it from the latest internal and push to the repository."
   git checkout -B "$PRIVATE_BRANCH" origin/internal
@@ -47,7 +49,7 @@ fi
 
 git fetch origin "$PRIVATE_BRANCH"
 # cut a merge branch at the latest public commit
-git checkout -B "$MERGE_BRANCH" "origin/$BRANCH"
+git checkout -B "$MERGE_BRANCH" "origin/$ORIGIN_BRANCH"
 
 # push to create new PR or update an existing one
 git push --force -u origin "$MERGE_BRANCH"
--- scripts/public-sync/ci-sync-with-public-repo.sh
+++ scripts/public-sync/ci-sync-with-public-repo.sh
@@ -2,8 +2,10 @@
 
 set -v
 set -euo pipefail
+set -x
 
 readonly BRANCH=$1
+PRIVATE_BRANCH="public/$BRANCH"
 
 if [[ -z $BRANCH ]]
 then
@@ -20,13 +22,13 @@ git remote set-url --push public-maps DISABLE
 git fetch public-maps "$BRANCH"
 
 # release branch won't exist after is cut for the first time
-PUBLIC_BRANCH_EXISTS=$(git ls-remote origin "$BRANCH" | wc -l)
+PUBLIC_BRANCH_EXISTS=$(git ls-remote origin "$PRIVATE_BRANCH" | grep -c "heads/$PRIVATE_BRANCH")
 if [[ $PUBLIC_BRANCH_EXISTS -eq 0 ]]; then
-  echo "Public $BRANCH doesn't exist in the private repo, cut it from the latest public and push to the private repo."
-  git checkout -B "$BRANCH" "public-maps/$BRANCH"
+  echo "Public $BRANCH doesn't exist in the private repo ($PRIVATE_BRANCH), cut it from the latest public and push to the private repo."
+  git checkout -B "$PRIVATE_BRANCH" "public-maps/$BRANCH"
 else
-  echo "Public $BRANCH exists in the private repo, merge it and push to the private repo."
-  git checkout -B "$BRANCH" "origin/$BRANCH"
+  echo "Public $BRANCH exists in the private repo ($PRIVATE_BRANCH), merge it and push to the private repo."
+  git checkout -B "$PRIVATE_BRANCH" "origin/$PRIVATE_BRANCH"
   git merge "public-maps/$BRANCH"
 fi
 
@@ -35,6 +37,6 @@ git remote remove public-maps
 # set access token associated with mapbox-ci user
 git remote set-url origin https://x-access-token:"$(mbx-ci github writer private token)"@github.com/mapbox/mapbox-maps-ios-private.git
 
-git push origin "$BRANCH"
+git push -u origin "$PRIVATE_BRANCH"
 
 git checkout "$COMMIT_SHA"
